ca1f2ee1e2e564e88ffe62a162d7990ec4643231
Andrew Risse <andrewrisse@gmail.com>
2021-07-26 23:34:51 -0600

    fix dockerfiles

--- backend/dist/backend/src/utils/decodeJsonWebToken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.decodeJsonWebToken = void 0;
-const jsonwebtoken_1 = require("jsonwebtoken");
-function decodeJsonWebToken(jwtToken) {
-    return jsonwebtoken_1.decode(jwtToken);
-}
-exports.decodeJsonWebToken = decodeJsonWebToken;
-//# sourceMappingURL=decodeJsonWebToken.js.map
