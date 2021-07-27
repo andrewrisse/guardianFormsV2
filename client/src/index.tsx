219dbcf0a92e4af456b733ad177c15c05ba48d5a
Andrew Risse <andrewrisse@gmail.com>
2021-07-26 22:08:56 -0600

    try changing frontend dockerfile to use nginx, add k8s files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,12 +7,12 @@ import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import { store } from "./redux/store";
 import { Auth0Provider } from '@auth0/auth0-react';
-const AUTH0_DOMAIN = "dev-vcyhieqy.us.auth0.com";
-const AUTH0_CLIENT_ID = "z3wGICauB28K5HorU1oHfD57k1QIXeZO";
-const AUTH0_REDIRECT_URL= "http://localhost:3000/dashboard";
+const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN_NAME
+const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID
+const AUTH0_REDIRECT_URL= process.env.REACT_APP_REDIRECT_URL
 
 ReactDOM.render(
-    <Auth0Provider domain={AUTH0_DOMAIN} clientId={AUTH0_CLIENT_ID} redirectUri={AUTH0_REDIRECT_URL}>
+    <Auth0Provider domain={AUTH0_DOMAIN!} clientId={AUTH0_CLIENT_ID!} redirectUri={AUTH0_REDIRECT_URL!}>
       <HelmetProvider>
         <React.StrictMode>
           <ReduxProvider store={store}>
